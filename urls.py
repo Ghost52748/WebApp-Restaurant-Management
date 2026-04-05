bffeede4e3becda3e03cb2fe5c030bd59a7c0225
GHost <anthonyjunang248@gmail.com>
2026-04-05 11:10:03 +0100

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,16 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    # Clients
-    path('clients/', views.client_list, name='client_list'),
-    path('clients/add/', views.client_create, name='client_create'),
-    path('clients/edit/<int:pk>/', views.client_update, name='client_update'),
-    path('clients/delete/<int:pk>/', views.client_delete, name='client_delete'),
-
-    # Products
-    path('products/', views.product_list, name='product_list'),
-    path('products/add/', views.product_create, name='product_create'),
-    path('products/edit/<int:pk>/', views.product_update, name='product_update'),
-    path('products/delete/<int:pk>/', views.product_delete, name='product_delete'),
+    path('admin/', admin.site.urls),
+    path('', include('core_app.urls')),  # toutes les URLs de core_app
 ]
